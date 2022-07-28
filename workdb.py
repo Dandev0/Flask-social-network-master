5022307601fa1bb2dd9b19793997d8307e978abc
Dandev0 <99640203+Dandev0@users.noreply.github.com>
2022-07-28 19:31:28 +0500

    Delete workdb.py

--- workdb.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-from flask import request, redirect
-
-base = sqlite3.connect('db/sc.db', check_same_thread=False)
-cur = base.cursor()
-
-r = cur.execute('SELECT*FROM data').fetchall()
-print(r)
